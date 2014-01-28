6060f419537d5a0f97e4790de3fc65f7f17a01c6
David Fisher <tibbon@gmail.com>
2014-01-28 15:40:34 -0500

    Added Go solution

--- 1_multiples_of_3_and_5/go/solve.go
+++ 1_multiples_of_3_and_5/go/solve.go
@@ -5,9 +5,11 @@ import "fmt"
 func main() {
 		solution := 0
 		i := 1
-    for i <= 10 {
-        fmt.Println(i)
+    for i < 1000 {
+        if i % 3 == 0 || i % 5 == 0 {
+          solution = solution + i
+        }
         i = i + 1
     }
-    fmt.Println("Solution")
+    fmt.Println(solution)
 }
